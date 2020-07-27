55f5f21aeefde949437125c8a898e2398a55cc73
Ashley <mayashley052@gmail.com>
2020-07-27 15:58:56 -0400

    changes made to decedant designs page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,45 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+// const queries = require("./src/constants/algolia")
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Design Shop`,
+    description: `Gatsby Airtable Example. Built using Airtable, Algolia Search, Gatsby Background Image plugin and  React Context API. Containts two sliders, real-time Airtable updates and submenus. Styled using Styled-Components. `,
+    author: `@johnsmilga`,
+    titleTemplate: `%s | Gatsby - Airtable`,
+    url: `https://gatsby-airtable-design-project.netlify.app/`,
+    image: `mainBcg.png`,
+    twitterUsername: `@john_smilga`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`400`, `500`, `700`],
+          },
+          {
+            family: `Open Sans`,
+          },
+          {
+            family: `Caveat`,
+          },
+        ],
+      },
+    },
+  ],
 }
